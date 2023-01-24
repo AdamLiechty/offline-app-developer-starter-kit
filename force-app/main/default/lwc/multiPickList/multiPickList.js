b73acd44a353bd616fe8f2c4ff766c0ea396a1e8
Adam Liechty <aliechty@salesforce.com>
2023-01-24 14:36:37 -0800

    multi pick list

--- force-app/main/default/lwc/multiPickList/multiPickList.js
+++ force-app/main/default/lwc/multiPickList/multiPickList.js
@@ -1,24 +1,44 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+import { QuestionResponseChangeEvent } from 'c/surveyEvents';
 
 export default class MultiPickList extends LightningElement {
-    value = 'inProgress';
+    @api question
+    @api response
 
-    @track options = [
-        { label: 'New', value: 'new' },
-        { label: 'In Progress', value: 'inProgress' },
-        { label: 'Finished', value: 'finished' },
-    ];
-
-    @track allValues = [];
-
-    handleChange(event) {
-        if(!this.allValues.includes(event.target.value)){
-            this.allValues.push(event.target.value)
-        }
+    get options() {
+        return this.question.choices.map(c => ({
+            value: c.Id,
+            label: c.Name
+        }));
     }
 
-    handleRemove(event) {
-        const valueRemoved = event.target.name;
-        this.allValues.splice(this.allValues.indexOf(valueRemoved),1)
+    get chosenOptions() {
+        const ids = this.response && this.response.value || [];
+        return this.options.filter(o => ids.includes(o.value));
+    }
+
+    handleChange(e) {
+        const choice = this.question.choices.find(c => c.Id === e.target.value);
+        if (!choice) return;
+        this.updateChoices(oldResponseValue => {
+            if (oldResponseValue.includes(choice.Id)) return oldResponseValue;
+            return [...oldResponseValue, choice.Id];
+        })
+    }    
+
+    handleRemove(e) {
+        this.updateChoices(oldResponseValue =>
+            oldResponseValue.filter(id => id !== e.target.name));
+    }
+
+    updateChoices(newIdsFn) {
+        const oldResponseValue = this.response && this.response.value || [];
+
+        // Remove the clicked pill's choice from the selection set.c/createAccountRecord
+        const value = newIdsFn(oldResponseValue);
+        const newChoices = this.question.choices.filter(c => value.includes(c.Id));
+        const displayValue = newChoices.map(c => c.Name);
+        const responseEvent = new QuestionResponseChangeEvent(this.question.Id, { value, displayValue });
+        this.dispatchEvent(responseEvent);
     }
 }
