f594f5d42074e213a88a36f9c3293e9a92a57d31
Adam Liechty <aliechty@salesforce.com>
2023-03-23 14:09:30 -0700

    survey response creation uses getRecordCreateDefaults and generateRecordInputForCreate

--- force-app/main/default/lwc/surveyRuntime/surveyRuntime.js
+++ force-app/main/default/lwc/surveyRuntime/surveyRuntime.js
@@ -1,23 +1,37 @@
-import { api, LightningElement } from 'lwc';
-import { createRecord } from 'lightning/uiRecordApi';
+import { api, wire, LightningElement } from "lwc";
+import {
+  createRecord,
+  getRecordCreateDefaults,
+  generateRecordInputForCreate,
+} from "lightning/uiRecordApi";
+import SURVEY_RESPONSE_OBJECT from "@salesforce/schema/OfflineSurveyResponse__c";
 
 export default class SurveyRuntime extends LightningElement {
-    @api recordId;
-    @api objectApiName;
+  @api recordId;
+  @api objectApiName;
 
-    handleResponseSubmit(event) {
-        const { surveyId, surveyVersionId, responses } = event.detail;
+  @wire(getRecordCreateDefaults, { objectApiName: SURVEY_RESPONSE_OBJECT })
+  surveyResponseCreateDefaults;
 
-        const responseObj = {
-            apiName: 'OfflineSurveyResponse__c',
-            fields: {
-                SurveyVersionId__c: surveyVersionId,
-                ResponseJSON__c: JSON.stringify(responses)
-            }
-        };
-        createRecord(responseObj).then(() => {
-            console.log('created OfflineSurveyResponse__c', responseObj);
-            history.back() // Close window.
-        });
-    }
+  handleResponseSubmit(event) {
+    const { surveyId, surveyVersionId, responses } = event.detail;
+
+    const defaults = this.surveyResponseCreateDefaults.data;
+    const objectInfo =
+      defaults.objectInfos[SURVEY_RESPONSE_OBJECT.objectApiName];
+    const record = {
+      apiName: defaults.record.apiName,
+      fields: {
+        ...defaults.record.fields,
+        SurveyVersionId__c: surveyVersionId,
+        ResponseJSON__c: JSON.stringify(responses),
+      },
+    };
+
+    const recordInput = generateRecordInputForCreate(record, objectInfo);
+    createRecord(recordInput).then(() => {
+      console.log("created OfflineSurveyResponse__c", recordInput);
+      history.back(); // Close window.
+    });
+  }
 }
